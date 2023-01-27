8b5289c1f3da8ede9d5ee8e4d688d0c3756fcc67
SophiaFavro <72363654+SophiaFavro@users.noreply.github.com>
2023-01-27 00:07:48 -0800

    added some setup for storing

--- studyBuddy/popup.js
+++ studyBuddy/popup.js
@@ -1,1 +1,27 @@
 console.log("Keep up the good work!");
+
+const list = document.querySelector("ul"); 
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    const userInput = input.value;
+    input.value = '';
+
+    console.log(userInput);
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listButton = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = userInput;
+    listItem.appendChild(listButton);
+    listButton.textContent = "Done";
+
+    list.appendChild(listItem);
+
+    listButton.addEventListener("click", () => {
+        list.removeChild(listItem);
+    });
+});
